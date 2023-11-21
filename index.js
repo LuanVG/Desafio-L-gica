757c76e3cfed1dc83d250ae7910af703c16ef1e2
LuanVG <luan_g.12@hotmail.com>
2023-11-21 12:38:16 -0300

    Modificação no código para a realização do desafio

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log('Ola, Mundo!')
+// CLASSIFICADOR DE NIVEL DE HEROI
+
+let nomeHeroi = 'Guardiao'
+let xpHeroi = 7000
+let nivelHeroi;
+
+
+switch (true) {
+  case xpHeroi <= 1000:
+    nivelHeroi = 'Ferro'
+    break
+
+  case xpHeroi >= 1001 && xpHeroi <= 2000:
+    nivelHeroi = 'Bronze'
+    break
+
+  case xpHeroi >= 2001 && xpHeroi <= 5000:
+    nivelHeroi = 'Prata'
+    break
+
+  case xpHeroi >= 6001 && xpHeroi <= 7000:
+    nivelHeroi = 'Ouro'
+    break
+
+  case xpHeroi >= 7001 && xpHeroi <= 8000:
+    nivelHeroi = 'Platina'
+    break
+
+  case xpHeroi >= 8001 && xpHeroi <= 9000:
+    nivelHeroi = 'Ascendente'
+    break
+
+  case xpHeroi >= 9001 && xpHeroi <= 10000:
+  nivelHeroi = 'Imortal'
+  break
+
+  case xpHeroi >= 10001:
+    nivelHeroi = 'Radiante'
+}
+
+console.log('O herói'+ ' ' + nomeHeroi + ' ' + 'está no nível de' + ' ' + nivelHeroi)
